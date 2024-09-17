be9f8b13b893829c9858e8cf5d09737eb8b54ed8
DxDanDxD <138779463+DxDanDxD@users.noreply.github.com>
2024-09-17 14:19:03 +0300

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,27 @@
+'''fio=input('Ваши фамилия, имя и отчество: ')
+age=input('Ваш возраст: ')
+address=input('Ваш адресс: ')'''
+
+import math
+
+'''x,t=10,1
+print(round(((9*math.pi*t+x*math.cos(x))/(1-abs(math.sin(t))))*(math.e**x),2))'''
+
+'''nomer 1 dz
+
+r = int(input())
+S, L = round(math.pi*r**2, 2), round(math.pi*r*2, 2)
+print(S, L)'''
+
+'''nomer 2 dz
+
+x,y = 10,55
+print(x,y)
+x,y = y,x
+print(x,y)'''
+
+'''nomer 3 dz'''
+
+g=int(9.81)
+L=int(input())
+print(round(2*math.pi*(L/g)**(1/2),2))

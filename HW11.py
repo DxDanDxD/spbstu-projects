43ac23c3b7ffe29bb346675c5a939625250e7439
Daniil Dityatev <danidity@gmail.com>
2024-12-10 03:10:06 +0300

    parampampam

---
+++ HW11.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+'''dz nomer 1'''
+
+f = pd.read_csv('./NISPUF17.csv', sep=',')
+def proportion_of_education(dataframe):
+    flen = len(f)
+    lt12 = round(float(f['EDUC1'].where(f['EDUC1'] == 1).count() / flen), 2)
+    e12 = round(float(f['EDUC1'].where(f['EDUC1'] == 2).count() / flen), 2)
+    mt12 = round(float(f['EDUC1'].where(f['EDUC1'] == 3).count() / flen), 2)
+    c = round(float(f['EDUC1'].where(f['EDUC1'] == 4).count() / flen), 2)
+    a = {"less than high school": lt12,
+            "high school": e12,
+            "more than high school but not college": mt12,
+            "college": c}
+    print(a)
+proportion_of_education(f)
+
+'''dz nomer 2'''
+
+f = pd.read_csv('./NISPUF17.csv', sep=',')
+f = f.dropna(subset=['P_NUMFLU'])
+a = f.groupby('CBF_01')['P_NUMFLU'].mean()
+print(round(float(a[1]), 1), round(float(a[2]), 1))
+
+'''dz nomer 3'''
+
+file = 'NISPUF17.csv'
+def chickenpox_by_sex(a):
+    f = pd.read_csv(a, sep=',')
+    print(f['HAD_CPOX'].unique())
+chickenpox_by_sex(file)
